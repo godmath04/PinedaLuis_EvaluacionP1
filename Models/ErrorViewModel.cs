ab3caa83edd9d252364b20a6ac0b6dbb3b70b3c4
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2025-04-29 07:13:40 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PinedaLuis_EvaluacionP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
